b08571d3fe50fcac02cf2a7a2b463746fd53d957
Skyggebjerg <ole@skyggebjerg.dk>
2015-01-20 22:20:58 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -149,7 +149,7 @@
 										width: 600,
 										height: 200,
 										renderer: 'line',
-										min: 0,
+										min: 10,
 										max: 30,
 										
 										//min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
